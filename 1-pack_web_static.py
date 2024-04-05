e1ea961ca7b49781c5ba467a57b909a53486ee1f
Desheye <aarealaafiaboyz@gmail.com>
2024-04-05 14:46:51 -0700

    pack

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,40 +1,24 @@
-<<<<<<< HEAD
 #!/usr/bin/python3
-"""Fabric script that generates a .tgz archive from the contents
-of the web_static folder of your AirBnB Clone repo."""
-
-from fabric.api import local
-from datetime import datetime
-
-
-def do_pack():
-    """Generates a .tgz archive from the contents of the web_static folder."""
-    try:
-        now = datetime.now().strftime('%Y%m%d%H%M%S')
-        local("mkdir -p versions")
-        filename = "versions/web_static_{}.tgz".format(now)
-        local("tar -cvzf {} web_static".format(filename))
-        return filename
-    except:
-        return None
-=======
-#!/usr/bin/python3
-# Fabfile to generates a .tgz archive from the contents of web_static.
+# This Fabfile generates a .tgz archive from the contents of the web_static directory.
 import os.path
 from datetime import datetime
 from fabric.api import local
 
-
 def do_pack():
-    """Create a tar gzipped archive of the directory web_static."""
+    """
+    Create a tar gzipped archive of the web_static directory.
+    """
     dt = datetime.utcnow()
-    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-        dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second
-    )
-    if os.path.isdir("versions") is False:
-        if local("mkdir -p versions").failed is True:
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
+                                                         dt.month,
+                                                         dt.day,
+                                                         dt.hour,
+                                                         dt.minute,
+                                                         dt.second)
+    if not os.path.isdir("versions"):
+        if local("mkdir -p versions").failed:
             return None
-    if local("tar -cvzf {} web_static".format(file)).failed is True:
+    if local("tar -cvzf {} web_static".format(file)).failed:
         return None
     return file
->>>>>>> 5ad838c611b22b1a462175ca002bcaaa1f0f54e3
+
