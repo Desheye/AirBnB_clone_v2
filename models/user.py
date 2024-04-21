9463206823f6b39b4e3daa8aabbfb12b230c3c2a
Desheye <aarealaafiaboyz@gmail.com>
2024-04-21 02:39:31 -0700

    user

--- models/user.py
+++ models/user.py
@@ -1,35 +1,29 @@
-from sqlalchemy import Column, String, DateTime  # Import DateTime here
-from sqlalchemy.orm import relationship
+#!/usr/bin/python3
+"""Defines the User class."""
+import models
 from models.base_model import BaseModel, Base
-from datetime import datetime  # If you are using this for default value
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    """Represents a user in the database.
-    Attributes:
-        email: The user's email address.
-        password: The user's password for login.
-        first_name: The user's first name.
-        last_name: The user's last name.
-    """
+    """Represents a user in the system."""
+    if models.storage_t == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
 
-    __tablename__ = "users"
-
-    id = Column(String(36), primary_key=True)
-    created_at = Column(DateTime, nullable=False,
-                        default=datetime.utcnow)
-    updated_at = Column(DateTime,
-                        nullable=False,
-                        default=datetime.utcnow,
-                        onupdate=datetime.utcnow)
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-
-    places = relationship("Place",
-                          cascade="all, delete, delete-orphan",
-                          backref="user")
-    reviews = relationship("Review",
-                           cascade="all, delete, delete-orphan",
-                           backref="user")
+    def __init__(self, *args, **kwargs):
+        """Initializes a user instance."""
+        super().__init__(*args, **kwargs)
